bb17abf484ab0e695a5eccf79b8d1b4c6d880208
Aidan Dang <aidandang@gmail.com>
2021-01-20 11:37:47 -0800

    fixing Order components

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -92,99 +92,28 @@ exports.readOrders = catchAsync(async (req, res, next) => {
 })
 
 exports.readOrderById = catchAsync(async (req, res, next) => {
-  const id = req.params.id
+  const { id } = req.params
 
-  // find the order with a provided id, 
-  // look up in item collection to get items of the order
-  // and return error if the order is not found
-  // otherwise return the order with items sorted by createdAt
-  const order = await Order.aggregate(getByIdAggr(id))
-
-  if (order.length === 0) return next(new AppError('No order found.', 404)) 
+  const order = await Order.findById(id)
   
   res.status(200).json({
     status: 'success',
-    byId: order[0]
+    byId: order
   })
 })
 
 exports.updateOrderById = catchAsync(async (req, res, next) => {
   const { id } = req.params
-  const { items } = req.body
-  const obj = { ...req.body }
-  delete obj.items
+  const reqBody = { ...req.body }
 
-  // before updating, the existing order needed to saved
-  // as a new revision
-  
-  // check if order found, return error if not
-  const found = await Order.findOne({ _id: id })
-
-  if (!found) return next(new AppError('No order found.', 404))
-
-  // get existing items
-  const existingItems = await Item.find({ orderNumber: found.orderNumber })
-
-  // update order
-  const updated = await Order.findByIdAndUpdate(
+  const updatedOrder = await Order.findByIdAndUpdate(
     id, 
-    obj, 
-    { runValidators: true }
+    reqBody, 
+    { new: true, runValidators: true }
   )
 
-  // update multile items to items collection
-  // if updating items have any item without _id then insert that item as a new one
-  // if existing items of the order have any item that updating items don't have 
-  // then delete that item
-  
-  const query = []
-  var i = 0
-
-  // add update and create operations to query
-  for (i = 0; i < items.length; i++) {
-    if (items[i]._id) {
-      query.push({
-        updateOne: {
-          "filter": { _id: items[i]._id },
-          "update": items[i]
-        }
-      })
-    } else {
-      query.push({
-        insertOne: {
-          "document": items[i]
-        }
-      })
-    }
-  }
-
-  // add delete operations to query
-  var j = 0
-  for (i = 0; i < existingItems.length; i++) {
-    var isDelete = true
-    for (j = 0; j < items.length; j++) {
-      if (existingItems[i]._id == items[j]._id) {
-        isDelete = false
-      }
-    }
-    if (isDelete) {
-      query.push({
-        deleteOne: {
-          "filter": { _id: existingItems[i]._id }
-        }
-      })
-    }
-  }
-
-  await Item.bulkWrite(query)
-
-  // get updated orders and response
-  const order = await Order.aggregate(getByIdAggr(id))
-
-  res
-    .status(200)
-    .json({
-      status: 'success',
-      byId: order[0]
-    })
+  res.status(200).json({
+    status: 'success',
+    byId: updatedOrder
+  })
 })
